0bc53f85fdf636bf6e124fe46a07370e5639ef58
KavinPrasad2948 <kavinprasad2948@gmail.com>
2024-03-13 16:27:25 +0530

    Completed

---
+++ js/script.js
@@ -0,0 +1,58 @@
+
+
+
+// Define countdown function using callback hell
+setTimeout(function() {
+    // Display countdown 10
+    document.body.innerHTML = '10';
+    
+    setTimeout(function() {
+        // Display countdown 9
+        document.body.innerHTML = '9';
+        
+        setTimeout(function() {
+            // Display countdown 8
+            document.body.innerHTML = '8';
+            
+            setTimeout(function() {
+                // Display countdown 7
+                document.body.innerHTML = '7';
+                
+                setTimeout(function() {
+                    // Display countdown 6
+                    document.body.innerHTML = '6';
+                    
+                    setTimeout(function() {
+                        // Display countdown 5
+                        document.body.innerHTML = '5';
+                        
+                        setTimeout(function() {
+                            // Display countdown 4
+                            document.body.innerHTML = '4';
+                            
+                            setTimeout(function() {
+                                // Display countdown 3
+                                document.body.innerHTML = '3';
+                                
+                                setTimeout(function() {
+                                    // Display countdown 2
+                                    document.body.innerHTML = '2';
+                                    
+                                    setTimeout(function() {
+                                        // Display countdown 1
+                                        document.body.innerHTML = '1';
+                                        
+                                        setTimeout(function() {
+                                            // Display Happy Independence Day
+                                            document.body.innerHTML= '<h1>Happy Independence Day!</h1>';
+                                        }, 1000); // Wait for 1 second
+                                    }, 1000); // Wait for 1 second
+                                }, 1000); // Wait for 1 second
+                            }, 1000); // Wait for 1 second
+                        }, 1000); // Wait for 1 second
+                    }, 1000); // Wait for 1 second
+                }, 1000); // Wait for 1 second
+            }, 1000); // Wait for 1 second
+        }, 1000); // Wait for 1 second
+    }, 1000); // Wait for 1 second
+}, 1000); // Wait for 1 second
